d9e567fc31fbaaa7795d0d14a10d6d088b13cf38
samh7 <samh74490@gmail.com>
2024-12-28 00:59:45 +0300

    apply auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { AuthGuardService } from './guards/auth-guard.service';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: LandingComponent,
+  },
+
+  {
+    path: "home",
+    component: HomeComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: "login",
+    component: LoginComponent,
+  },
+  {
+    path: "signup",
+    component: SignupComponent,
+  },
+  {
+    path: "*",
+    component: HomeComponent,
+    canActivate: [AuthGuardService]
+  },
+
+  {
+    path: "**",
+    component: HomeComponent,
+    canActivate: [AuthGuardService]
+  },
+];
