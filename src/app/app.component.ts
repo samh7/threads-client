6bbf9773841265e6d3473ba4413dc48dc797230a
samh7 <samh74490@gmail.com>
2024-12-28 01:01:04 +0300

    add header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, resource, signal } from '@angular/core';
+import { NavigationStart, Router, RouterOutlet, Event as NavigationEvent, NavigationEnd } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { single, Subscription } from 'rxjs';
+import { routes } from './app.routes';
+// import { authGuard } from './guards/auth.guard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
-  title = 'threads-client';
+  headerFooterClasses = signal("")
+  router = inject(Router)
+  showComponents = signal("")
+
+  constructor() {
+    this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+
+        if (val.url.includes("/login")
+          || val.url.includes("/signup")
+        ) {
+          this.showComponents.set("hidden")
+        }
+        else {
+          this.showComponents.set("")
+
+        }
+      }
+    })
+  }
+
 }
