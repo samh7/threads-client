ac30e3764f96cc86986abd1b944506499e798730
samh7 <samh74490@gmail.com>
2024-12-28 00:55:32 +0300

    add footer component

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styles: ``
+})
+export class FooterComponent {
+  classes = input("")
+}
